0185f20e18de4326e9179045c0dd5a79f0d9e4df
Tom Brückner <tom.brueckner@gmx.net>
2016-09-21 13:31:46 +0200

    stripped unnecessary code

--- scripts/ios10-patches.js
+++ scripts/ios10-patches.js
@@ -9,48 +9,13 @@ var
   path = require("path"),
 
   OLD_VIEW_CONTROLLER_PATH = '/platforms/ios/CordovaLib/Classes/CDVViewController.m',
-  NEW_VIEW_CONTROLLER_PATH = '/plugins/io.guzz.cordova-ios10-patches/patches/CDVViewController.m',
-  COMMENT_KEY = /_comment$/;
-
-
-// Helpers
-
-// Returns the project name
-function getProjectName(protoPath) {
-  var
-    cordovaConfigPath = path.join(protoPath, 'config.xml'),
-    content = fs.readFileSync(cordovaConfigPath, 'utf-8');
-
-  return /<name>([\s\S]*)<\/name>/mi.exec(content)[1].trim();
-}
-
-// Drops the comments
-function nonComments(obj) {
-  var
-    keys = Object.keys(obj),
-    newObj = {},
-    i = 0;
-
-  for (i; i < keys.length; i += 1) {
-    if (!COMMENT_KEY.test(keys[i])) {
-      newObj[keys[i]] = obj[keys[i]];
-    }
-  }
-
-  return newObj;
-}
-
-
-// Starting here
+  NEW_VIEW_CONTROLLER_PATH = '/plugins/io.guzz.cordova-ios10-patches/patches/CDVViewController.m';
 
 module.exports = function(context) {
   var
-    xcode = context.requireCordovaModule('xcode'),
     projectRoot = context.opts.projectRoot,
-    projectName = getProjectName(projectRoot),
     oldViewControllerPath = path.join(projectRoot, OLD_VIEW_CONTROLLER_PATH),
-    newViewControllerPath = path.join(projectRoot, NEW_VIEW_CONTROLLER_PATH),
-    xcodeProject;
+    newViewControllerPath = path.join(projectRoot, NEW_VIEW_CONTROLLER_PATH);
 
   // Check if old view controller is existing
   if (!fs.existsSync(oldViewControllerPath)) {
@@ -69,11 +34,9 @@ module.exports = function(context) {
   fs.createReadStream(newViewControllerPath).pipe(fs.createWriteStream(oldViewControllerPath));
 };
 
-
 function debug(msg) {
   console.log('ios10-patches.js [INFO] ' + msg);
 }
-
 
 function debugerror(msg) {
   console.error('ios10-patches.js [ERROR] ' + msg);
